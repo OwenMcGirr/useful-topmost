2ef34596a85c6705f26dd28218f6cfd4ebfa1406
Owen McGirr <o.a.mcgirr@gmail.com>
2026-05-25 16:15:20 +0100

    chore: remove debug probe scripts

--- probe-electron.js
+++
@@ -1,7 +0,0 @@
-const electron = require('electron');
-console.log('typeof electron:', typeof electron);
-console.log('electron keys:', electron && typeof electron === 'object' ? Object.keys(electron) : '(not an object)');
-console.log('electron.app:', electron && electron.app);
-console.log('electron.default:', electron && electron.default && typeof electron.default);
-console.log('process.versions.electron:', process.versions.electron);
-process.exit(0);
--- probe-electron.mjs
+++
@@ -1,5 +0,0 @@
-import * as electron from 'electron';
-console.log('keys:', Object.keys(electron));
-console.log('app:', electron.app && typeof electron.app);
-console.log('default keys:', electron.default ? Object.keys(electron.default) : '(no default)');
-process.exit(0);
